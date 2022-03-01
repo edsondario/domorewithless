614a3d4e22a544bd2508c3c73c0c16fd32d11b86
edsondario <edson.dario@gmail.com>
2022-02-28 22:02:52 -0300

    add navbar_link to afastamentos

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'afastamentos/index'
-  get 'afastamentos/create'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'afastamentos/', to: 'afastamentos#index'
+  post 'afastamentos/', to: 'afastamentos#create'
 end
