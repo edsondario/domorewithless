f52bce13fd25b40be970fe629395da5b379317ec
edsondario <edson.dario@gmail.com>
2022-03-01 15:07:22 -0300

    adjust afastamentos#create

--- app/controllers/afastamentos_controller.rb
+++ app/controllers/afastamentos_controller.rb
@@ -7,15 +7,26 @@ class AfastamentosController < ApplicationController
   end
   
   def create
-    id_orgao = format('%05d', params[:afastamentos][:id_orgao])
-    datasrc = params[:afastamentos][:datasrc]
-    lines = format('%06d', CSV.read(datasrc.path).count)
+    datasrc = params[:leaves][:datasrc]
+    government_body = format('%05d', params[:leaves][:government_body])
+    manager_cpf = format('%011d', params[:leaves][:manager_cpf].to_i)
+    reference_month = params[:leaves][:reference_month]
+    lines = "9#{format('%06d', CSV.read(datasrc.path).count)}"
 
-    datadst = "0#{id_orgao}" + "\n"
+    datadst = "0#{government_body}" + reference_month + "000" + "\n"
     CSV.foreach(datasrc.path, headers: false) do |row|
-      datadst += "#{id_orgao}#{format('%07d',row[0].to_i)}#{row[1]}#{format('%04d',row[2].to_i)}" + "\n"
+      siape = format('%07d',row[0].to_i)
+      date_leave = "#{row[1][4..7]}#{row[1][2..3]}#{row[1][0..1]}"
+      id_medical_leave = format('%04d',row[2].to_i)
+
+      datadst += "1#{manager_cpf}#{government_body}#{siape}#{date_leave}#{date_leave}#{id_medical_leave}" + "\n"
     end
-    datadst += "9#{lines}"
-    send_data("#{datadst}", :type => 'text/plain', :disposition => 'attachment', :filename => "carga_batch_afastamentos-#{id_orgao}-.txt")
-  end
+    datadst += lines
+    
+    file_timestamp = "#{Date.current.strftime("%d%m%Y")}-#{Time.current.strftime("%H%M")}"
+    send_data("#{datadst}",
+      :type => 'text/plain',
+      :disposition => 'attachment',
+      :filename => "load_batch_covid-19-#{government_body}-#{file_timestamp}.txt")
+    end
 end
