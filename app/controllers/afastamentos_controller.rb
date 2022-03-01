0ebfc418412e60e0e7532227f017b24090c4bf97
edsondario <edson.dario@gmail.com>
2022-02-28 22:36:59 -0300

    add index and create to afastamentos

--- app/controllers/afastamentos_controller.rb
+++ app/controllers/afastamentos_controller.rb
@@ -1,9 +1,21 @@
 class AfastamentosController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :create ]
   
+  require 'csv'
+
   def index
   end
-
+  
   def create
+    id_orgao = format('%05d', params[:afastamentos][:id_orgao])
+    datasrc = params[:afastamentos][:datasrc]
+    lines = format('%06d', CSV.read(datasrc.path).count)
+
+    datadst = "0#{id_orgao}" + "\n"
+    CSV.foreach(datasrc.path, headers: false) do |row|
+      datadst += "#{id_orgao}#{format('%07d',row[0].to_i)}#{row[1]}#{format('%04d',row[2].to_i)}" + "\n"
+    end
+    datadst += "9#{lines}"
+    send_data("#{datadst}", :type => 'text/plain', :disposition => 'attachment', :filename => "carga_batch_afastamentos-#{id_orgao}-.txt")
   end
 end
